fed0b298b0612c24e51001cacbaa6559ef4a970c
Alexander Tejedor <atromero.98@gmail.com>
2024-07-06 21:03:43 -0500

    limpie mediante js los elementos de html al clickear sobre el boton, tambien agrege nuevos elementos mediante JavaScript

--- main.js
+++ main.js
@@ -1,14 +1,36 @@
 // Seleccionar los datos del DOM
 const btn = document.getElementById('card-btn');
-const container = document.querySelector('card-container');
 
-// Funcion para eliminar los elementos del DOM
+// Funcion que se ejecuta al hacer click en el boton
 var deleteElement = btn.addEventListener ('click', () => {
-    const cardContent = document.getElementById('card-content');
-    cardContent.removeChild(btn);
+    //Limpiar el contenedor card-content
+    const container = document.getElementById('card-content');
+    container.innerHTML = '';
+
+    //Agregar elementos al DOM
+    const figure = document.createElement('figure');
+    container.appendChild(figure);
+
+    const img = document.querySelector('figure');
+    img.innerHTML = '<img src="./assets/images/illustration-thank-you.svg" alt="Image">';
+
+    const span = document.createElement('span');
+    span.textContent = 'You selected 4 out of 5';
+    container.appendChild(span);
+
+    const title = document.createElement('h1');
+    title.textContent = 'Thank you!';
+    container.appendChild(title);
+
+    const description = document.createElement('p');
+    description.textContent = `We appreciate you taking the time to give a rating. if your ever need more support. don't hesitate to get in touch!`;
+    container.appendChild(description);
+
+    //Agregar clases a los elementos
+    title.classList.add('card-title-two');
+    description.classList.add('card-description-two')
+    figure.classList.add('card-image')
+    span.classList.add('selection')
+    //Reemplazar elementos del DOM
 })
 
-// Agregar elementos al DOM
-var addElement = btn.addEventListener ('click', () =>{
-    
-})
